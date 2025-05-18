aa2e738a1c74a3d181ef08258a2ac22981192a24
Aidan Wiseman <aidanwiseman@outlook.com>
2025-05-18 18:16:28 +0100

    support filepaths by default instead of an undifferentiated string

--- sanitise_filename.cpp
+++ sanitise_filename.cpp
@@ -10,7 +10,22 @@ int main(int argc, char** argv)
         std::cerr << "no filename provided\n";
         return EXIT_FAILURE;
     }
-    std::string filename { argv[1] };
-    std::cout << std::format("original filename: \"{}\"\n", filename);
+    std::string filepath { argv[1] };
+    // split filepath into chunks for each dir
+    std::vector<std::string> filepath_split {};
+    auto left { filepath.begin() };
+    auto it_end { filepath.end() };
+    auto right = it_end;
+
+    while ((right = std::find(left, it_end, '/')) != it_end) {
+        filepath_split.push_back({left, right});
+        left = right + 1;
+    }
+    filepath_split.push_back({left, right});
+
+
+    // choose the last chunk as the filename
+    std::string& filename {*(filepath_split.end()-1)};
+    fmt::print("{:s}\n", fmt::join(filepath_split, "/"));
     return EXIT_SUCCESS;
 }
