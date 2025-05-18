662eb4655ce2d109aa682d8e4cf2c1e3d7764de7
Aidan Wiseman <aidanwiseman@outlook.com>
2025-05-18 18:17:37 +0100

    don't modify file extensions if present

--- sanitise_filename.cpp
+++ sanitise_filename.cpp
@@ -11,6 +11,7 @@ int main(int argc, char** argv)
         return EXIT_FAILURE;
     }
     std::string filepath { argv[1] };
+    bool no_ext {false};
     // split filepath into chunks for each dir
     std::vector<std::string> filepath_split {};
     auto left { filepath.begin() };
@@ -26,6 +27,18 @@ int main(int argc, char** argv)
 
     // choose the last chunk as the filename
     std::string& filename {*(filepath_split.end()-1)};
+
+    // extract the file extension
+    std::string extension {};
+    auto ext_pos { filename.rfind('.', filename.size()-1) };
+    if (ext_pos == filename.npos)
+        no_ext = true;
+    else
+        extension = filename.substr(ext_pos+1, filename.size() -1);
+    // remove file extension from the filename
+    filename = filename.substr(0, ext_pos);
+    if (!no_ext)
+        filename += '.' +  extension;
     fmt::print("{:s}\n", fmt::join(filepath_split, "/"));
     return EXIT_SUCCESS;
 }
