5ca63b16371181669ce75ae76d3866699eb4b3d6
Aidan Wiseman <aidanwiseman@outlook.com>
2025-05-18 18:12:57 +0100

    use cstdlib exit status macros

--- sanitise_filename.cpp
+++ sanitise_filename.cpp
@@ -8,8 +8,9 @@ int main(int argc, char** argv)
 {
     if (argc < 2) {
         std::cerr << "no filename provided\n";
-        return 1;
+        return EXIT_FAILURE;
     }
     std::string filename { argv[1] };
     std::cout << std::format("original filename: \"{}\"\n", filename);
+    return EXIT_SUCCESS;
 }
